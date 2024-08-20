5e614f707d4c12987930b577171f49b1639f9829
VSPCODERyt <165260098+VSPCODERyt@users.noreply.github.com>
2024-08-20 11:34:04 +0530

    Create theme.js

---
+++ theme.js
@@ -0,0 +1,21 @@
+export const heliactylTheme = {
+  palette: {
+    primary: {
+      main: '#3498db',
+    },
+    secondary: {
+      main: '#f1c40f',
+    },
+  },
+  typography: {
+    fontFamily: 'Open Sans',
+  },
+  overrides: {
+    MuiCard: {
+      root: {
+        boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        borderRadius: 10,
+      },
+    },
+  },
+};
